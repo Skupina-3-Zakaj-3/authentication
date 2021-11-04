74a858850aac9c53b214738678746118448a8b09
Anze <anze603@gmail.com>
2021-11-04 23:16:13 +0100

    added axios, http post request, config

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [HttpModule],
   providers: [UsersService],
   exports: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,36 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GoogleUser } from 'src/auth/auth.service';
 
 @Injectable()
 export class UsersService {
+  endpoint: string;
+
+  constructor(
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {
+    this.endpoint = `${this.configService.get<string>(
+      'services.user.endpoint',
+    )}:${this.configService.get(
+      'services.user.port',
+    )}/${this.configService.get<string>('services.user.version')}/users`;
+    console.log(this.endpoint);
+  }
   /**
    * Checks if google user exists in our database and creates if it doesn't
    * @param googleUser user to be checked if exists or to be created
    * @returns Google user of our app
    */
   async findOrCreteGoogleUser(googleUser: GoogleUser): Promise<GoogleUser> {
+    let user;
+    try {
+      user = await this.httpService.post(this.endpoint, googleUser).toPromise();
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
     return null;
   }
 }
