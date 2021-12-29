7e7976e557695ae846e4f4ea9c372d23701a808d
Anze <anze603@gmail.com>
2021-12-29 14:27:13 +0100

    google token in own window

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,12 @@
 import { GoogleAuthGuard } from './google-auth.guard';
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Response,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService, GoogleUser } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 
@@ -18,18 +25,31 @@ export class AuthController {
 
   @Get('google/redirect')
   @UseGuards(GoogleAuthGuard)
-  async googleAuthRedirect(@Request() req) {
+  async googleAuthRedirect(@Request() req, @Response() res) {
     const googleUser = await this.userService.findOrCreateGoogleUser(
       req.user as GoogleUser,
     );
 
-    if (googleUser) {
+    const acces_token = googleUser
+      ? this.authService.createJwt(googleUser.id, googleUser.email)
+      : '';
+    /* if (googleUser) {
       return {
         access_token: this.authService.createJwt(
           googleUser.id,
           googleUser.email,
         ),
       };
-    }
+    } */
+
+    let responseHTML =
+      '<html><head><title>Main</title></head><body></body><script>res = %value%; window.opener.postMessage(res, "*");window.close();</script></html>';
+    responseHTML = responseHTML.replace(
+      '%value%',
+      JSON.stringify({
+        acess_token: acces_token,
+      }),
+    );
+    res.status(HttpStatus.OK).send(responseHTML);
   }
 }
